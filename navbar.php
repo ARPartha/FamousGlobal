3edb2a2b35cd9bb70d13a69c4eea3723ac37b6ce
A.R partha <abrahmanpartha@gmail.com>
2020-03-13 21:46:21 +0600

    updated

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<ul class="navbar-nav mr-auto">
+	        	<li class="nav-item active"><a href="index.php" class="nav-link pl-0">Home</a></li>
+	        	<li class="nav-item"><a href="about.php" class="nav-link">Start Here</a></li>
+	        	<li class="nav-item"><a href="courses.php" class="nav-link">Programs</a></li>
+	        	<li class="nav-item"><a href="teacher.php" class="nav-link">Services</a></li>
+	        	<li class="nav-item"><a href="blog.php" class="nav-link">About it</a></li>
+	          <li class="nav-item"><a href="contact.php" class="nav-link">Contact Us</a></li>
+	        </ul>
