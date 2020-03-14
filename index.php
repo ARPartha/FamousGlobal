560cc92b090a73a83307685a34c04263cbea9b9c
A.R partha <abrahmanpartha@gmail.com>
2020-03-14 15:00:42 +0600

    updated homepage

--- index.php
+++ index.php
@@ -159,7 +159,7 @@
 						<div class="img" style="background-image: url(images/about.jpg); border"></div>
 					</div>
 					<div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
-          	<h2 class="mb-4">Why Aliff</h2>
+          	<h2 class="mb-4">Why Famous GLobal</h2>
 						<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
 						<div class="row mt-5">
 							<div class="col-lg-6">
@@ -236,7 +236,7 @@
     				</div>
     			</div>
           <div class="col-md-6 heading-section heading-section-white ftco-animate pl-lg-5 pt-md-0 pt-5">
-            <h2 class="mb-4">Fox University</h2>
+            <h2 class="mb-4">Famous Global Education</h2>
             <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
           </div>
@@ -291,61 +291,63 @@
 			<div class="container-fluid px-4">
 				<div class="row justify-content-center mb-5 pb-2">
           <div class="col-md-8 text-center heading-section ftco-animate">
-            <h2 class="mb-4"><span>Our</span> Courses</h2>
-            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            <h2 class="mb-4"><span>Our</span>Services</h2>
+            <p></p>
           </div>
         </div>	
 				<div class="row">
 					<div class="col-md-3 course ftco-animate">
 						<div class="img" style="background-image: url(images/course-1.jpg);"></div>
 						<div class="text pt-4">
-							<p class="meta d-flex">
-								<span><i class="icon-user mr-2"></i>Mr. Khan</span>
-								<span><i class="icon-table mr-2"></i>10 seats</span>
-								<span><i class="icon-calendar mr-2"></i>4 Years</span>
-							</p>
-							<h3><a href="#">Electric Engineering</a></h3>
-							<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-							<p><a href="#" class="btn btn-primary">Apply now</a></p>
+							<h3><a href="#">Career Counseling</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/image_4.jpg);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">Admission</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/bg_2.jpg);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">Foreign Languages</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/visa.jpg);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">Visa</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/image_5.jpg);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">Standardized Tests</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/education-loan.jpg);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">Education Loan</a></h3>
+						</div>
+					</div>
+					<div class="col-md-3 course ftco-animate">
+						<div class="img" style="background-image: url(images/Fitness.png);"></div>
+						<div class="text pt-4">
+							<h3><a href="#">International Fitness</a></h3>
 						</div>
 					</div>
 					<div class="col-md-3 course ftco-animate">
 						<div class="img" style="background-image: url(images/course-2.jpg);"></div>
 						<div class="text pt-4">
-							<p class="meta d-flex">
-								<span><i class="icon-user mr-2"></i>Mr. Khan</span>
-								<span><i class="icon-table mr-2"></i>10 seats</span>
-								<span><i class="icon-calendar mr-2"></i>4 Years</span>
-							</p>
-							<h3><a href="#">Electric Engineering</a></h3>
-							<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-							<p><a href="#" class="btn btn-primary">Apply now</a></p>
+							<h3><a href="#">International Grooming</a></h3>
 						</div>
 					</div>
 					<div class="col-md-3 course ftco-animate">
-						<div class="img" style="background-image: url(images/course-3.jpg);"></div>
+						<div class="img" style="background-image: url(images/scholarships.jpg);"></div>
 						<div class="text pt-4">
-							<p class="meta d-flex">
-								<span><i class="icon-user mr-2"></i>Mr. Khan</span>
-								<span><i class="icon-table mr-2"></i>10 seats</span>
-								<span><i class="icon-calendar mr-2"></i>4 Years</span>
-							</p>
-							<h3><a href="#">Electric Engineering</a></h3>
-							<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-							<p><a href="#" class="btn btn-primary">Apply now</a></p>
-						</div>
-					</div>
-					<div class="col-md-3 course ftco-animate">
-						<div class="img" style="background-image: url(images/course-4.jpg);"></div>
-						<div class="text pt-4">
-							<p class="meta d-flex">
-								<span><i class="icon-user mr-2"></i>Mr. Khan</span>
-								<span><i class="icon-table mr-2"></i>10 seats</span>
-								<span><i class="icon-calendar mr-2"></i>4 Years</span>
-							</p>
-							<h3><a href="#">Electric Engineering</a></h3>
-							<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-							<p><a href="#" class="btn btn-primary">Apply now</a></p>
+							<h3><a href="#">Scholarship</a></h3>
 						</div>
 					</div>
 				</div>
@@ -453,8 +455,8 @@
     			<div class="col-md-6 py-5 px-md-5">
     				<div class="py-md-5">
 		          <div class="heading-section heading-section-white ftco-animate mb-5">
-		            <h2 class="mb-4">Request A Quote</h2>
-		            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+		            <h2 class="mb-4">GET IN TOUCH</h2>
+		            <p>Use following form to email us.We will connect you instantly.</p>
 		          </div>
 		          <form action="#" class="appointment-form ftco-animate">
 		    				<div class="d-md-flex">
@@ -469,29 +471,24 @@
 		    					<div class="form-group">
 			    					<div class="form-field">
 		        					<div class="select-wrap">
-		                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-		                    <select name="" id="" class="form-control">
-		                    	<option value="">Select Your Course</option>
-		                      <option value="">Art Lesson</option>
-		                      <option value="">Language Lesson</option>
-		                      <option value="">Music Lesson</option>
-		                      <option value="">Sports</option>
-		                      <option value="">Other Services</option>
-		                    </select>
-		                  </div>
-			              </div>
-			    				</div>
-		    					<div class="form-group ml-md-4">
+									<div class="form-group ml-md-4">
 			    					<input type="text" class="form-control" placeholder="Phone">
 			    				</div>
 		    				</div>
 		    				<div class="d-md-flex">
 		    					<div class="form-group">
 			              <textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Message"></textarea>
+						  <br>
+						  <br>
+						  <div class="form-group ml-md-4">
+			              <input type="submit" value="Send" class="btn btn-primary py-3 px-4">
 			            </div>
-			            <div class="form-group ml-md-4">
-			              <input type="submit" value="Request A Quote" class="btn btn-primary py-3 px-4">
 			            </div>
+		                    </div>
+			              </div>
+			    				</div>
+		    					
+			            
 		    				</div>
 		    			</form>
 		    		</div>
@@ -504,79 +501,123 @@
 			<div class="container">
 				<div class="row justify-content-center mb-5 pb-2">
           <div class="col-md-8 text-center heading-section ftco-animate">
-            <h2 class="mb-4"><span>Recent</span> Blog</h2>
-            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            <h2 class="mb-4"><span>COUNTRIES</span> WE CONSULT</h2>
+            <p></p>
           </div>
         </div>
 				<div class="row">
           <div class="col-md-6 col-lg-4 ftco-animate">
             <div class="blog-entry">
-              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_1.jpg');">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
 								<div class="meta-date text-center p-2">
-                  <span class="day">26</span>
-                  <span class="mos">June</span>
-                  <span class="yr">2019</span>
                 </div>
               </a>
               <div class="text bg-white p-4">
-                <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-lg-4 ftco-animate">
+		  <div class="col-md-6 col-lg-4 ftco-animate">
             <div class="blog-entry">
-              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_2.jpg');">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
 								<div class="meta-date text-center p-2">
-                  <span class="day">26</span>
-                  <span class="mos">June</span>
-                  <span class="yr">2019</span>
                 </div>
               </a>
               <div class="text bg-white p-4">
-                <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-lg-4 ftco-animate">
+		  <div class="col-md-6 col-lg-4 ftco-animate">
             <div class="blog-entry">
-              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_3.jpg');">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
 								<div class="meta-date text-center p-2">
-                  <span class="day">26</span>
-                  <span class="mos">June</span>
-                  <span class="yr">2019</span>
                 </div>
               </a>
               <div class="text bg-white p-4">
-                <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
               </div>
             </div>
           </div>
-        </div>
+		<div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
+		  <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/sydney.jpg');">
+								<div class="meta-date text-center p-2">
+                </div>
+              </a>
+              <div class="text bg-white p-4">
+                <h3 class="heading"><a href="#"><strong>Australia</strong></a></h3>
+              </div>
+            </div>
+          </div>
 			</div>
+      </div>
 		</section>
 
 		<section class="ftco-section testimony-section">
